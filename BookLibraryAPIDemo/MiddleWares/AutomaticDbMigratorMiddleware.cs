12ab0db1a276e9668616450e9cf91bb112cdbdf4
pla <adad@asdasd.csd>
2025-03-22 02:27:00 +0330

    feat: add middleware

--- BookLibraryAPIDemo/MiddleWares/AutomaticDbMigratorMiddleware.cs
+++ BookLibraryAPIDemo/MiddleWares/AutomaticDbMigratorMiddleware.cs
@@ -1,17 +1,20 @@
+using BookLibraryAPIDemo.Infrastructure.Context;
+using Microsoft.EntityFrameworkCore;
+
 namespace BookLibraryAPIDemo.MiddleWares;
 
 public class AutomaticDbMigratorMiddleware
 {
     private readonly RequestDelegate _next;
 
-    public AutomaticDatabaseMigratorMiddleware(RequestDelegate next)
+    public AutomaticDbMigratorMiddleware(RequestDelegate next)
     {
         _next = next;
     }
     
     public async Task InvokeAsync(HttpContext httpContext)
     {
-        var db = httpContext.RequestServices.GetRequiredService<CleanArchitecturesDbContext>();
+        var db = httpContext.RequestServices.GetRequiredService<BookLibraryContext>();
         if (db.Database.IsRelational())
         {
             await db.Database.MigrateAsync(httpContext.RequestAborted);
