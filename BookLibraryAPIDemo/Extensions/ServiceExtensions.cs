898cee1d2469db14a6ac1cb0164b44c8307c97a3
pla <adad@asdasd.csd>
2025-03-22 19:37:04 +0330

    chore: remove extra codes

--- BookLibraryAPIDemo/Extensions/ServiceExtensions.cs
+++ BookLibraryAPIDemo/Extensions/ServiceExtensions.cs
@@ -20,8 +20,7 @@ namespace BookLibraryAPIDemo.API.Extensions
                 options => options.AddPolicy(
                     "CorsPolicy",
                     policy => policy.WithOrigins(
-                        configuration["BackendUrl"] ?? "https://localhost:5052",
-                        configuration["FrontendUrl"] ?? "https://localhost:7224")
+                        configuration["BackendUrl"] ?? "https://localhost:5052")
                         .AllowAnyMethod()
                         .AllowAnyHeader()
                          .AllowCredentials()
@@ -66,35 +65,5 @@ namespace BookLibraryAPIDemo.API.Extensions
                 };
             });
         }
-        public static void ConfigureExceptionHandler(this WebApplication app, ILoggerManager logger)
-        {
-            app.UseExceptionHandler(appError =>
-            {
-                appError.Run(async context =>
-                {
-                    context.Response.ContentType = "application/json";
-
-                    var contextFeature = context.Features.Get<IExceptionHandlerFeature>();
-                    if (contextFeature != null)
-                    {
-                        context.Response.StatusCode = contextFeature.Error switch
-                        {
-                            NotFoundException => StatusCodes.Status404NotFound,
-                            _ => StatusCodes.Status500InternalServerError
-                        };
-
-                        logger.LogError($"Something went wrong: {contextFeature.Error}");
-
-                        await context.Response.WriteAsync(new ErrorDetails
-                        {
-                            StatusCode = context.Response.StatusCode,
-                            Message = contextFeature.Error.Message,
-                        }.ToString());
-                    }
-                });
-            });
-        }
-
-
     }
 }
