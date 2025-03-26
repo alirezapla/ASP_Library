77c46a6760742a661515ab36a0fe6e7b5720e0a6
pla <adad@asdasd.csd>
2025-03-26 16:00:34 +0330

    fix: book wont be update

--- BookLibraryAPIDemo/Application/DTO/Book/UpdateBookDTO.cs
+++ BookLibraryAPIDemo/Application/DTO/Book/UpdateBookDTO.cs
@@ -2,7 +2,6 @@ namespace BookLibraryAPIDemo.Application.DTO.Book
 {
     public class UpdateBookDTO
     {
-        public string Title { get; set; }
         public string AuthorId { get; set; }
         public string CategoryId { get; set; }
         public string PublisherId { get; set; }
