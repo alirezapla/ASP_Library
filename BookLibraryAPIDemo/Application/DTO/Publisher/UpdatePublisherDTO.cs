00d6e9e648046f2d988b1343fcb05e2cc588136b
pla <adad@asdasd.csd>
2025-03-26 16:01:03 +0330

    fix: Publisher's Name could not be changed

--- BookLibraryAPIDemo/Application/DTO/Publisher/UpdatePublisherDTO.cs
+++ BookLibraryAPIDemo/Application/DTO/Publisher/UpdatePublisherDTO.cs
@@ -2,7 +2,6 @@
 {
     public class UpdatePublisherDTO
     {
-        public string PublisherName { get; set; }
         public string Address { get; set; }
         public string City { get; set; }
         public string Country { get; set; }
