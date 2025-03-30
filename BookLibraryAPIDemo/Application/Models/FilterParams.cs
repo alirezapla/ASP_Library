d79fa630d07ba555bf1ccd3a63f742c816496c3c
pla <adad@asdasd.csd>
2025-03-31 02:36:42 +0330

    feat: add filter param Schema

--- BookLibraryAPIDemo/Application/Models/FilterParams.cs
+++ BookLibraryAPIDemo/Application/Models/FilterParams.cs
@@ -2,5 +2,7 @@ namespace BookLibraryAPIDemo.Application.Models;
 
 public class FilterParams
 {
-    
+    public required string PropertyName { get; set; }
+    public required string PropertyValue { get; set; }
+    public required string Operator { get; set; }
 }
