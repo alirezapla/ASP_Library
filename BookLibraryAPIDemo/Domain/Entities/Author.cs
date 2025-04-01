949ad0e3596803674c4ba8ca64b9236342dd319a
pla <adad@asdasd.csd>
2025-04-01 15:33:39 +0330

    chore: refactor books type

--- BookLibraryAPIDemo/Domain/Entities/Author.cs
+++ BookLibraryAPIDemo/Domain/Entities/Author.cs
@@ -1,12 +1,10 @@
 ﻿namespace BookLibraryAPIDemo.Domain.Entities
 {
-
     public class Author : BaseEntity
     {
         public string Name { get; set; }
         public string Email { get; set; }
         public string Bio { get; set; }
-        public List<Book> Books { get; set; }
+        public ICollection<Book> Books { get; set; }
     }
-
 }
